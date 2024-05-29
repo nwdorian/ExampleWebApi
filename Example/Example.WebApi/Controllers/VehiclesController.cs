91b6a29f4c81bfdbe1767e84fb5b528a993a4d4d
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-05-29 11:28:58 +0200

    add getall and get endpoints

---
+++ Example/Example.WebApi/Controllers/VehiclesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class VehiclesController : ControllerBase
+{
+    private static readonly List<Vehicle> _vehicles = new List<Vehicle>
+    {
+        new Vehicle { Id = 1, Manufacturer = "Bmw", Model = "530", Color = "Black", HorsePower = 250},
+        new Vehicle { Id = 2, Manufacturer = "Mercedes", Model = "GLA", Color = "Grey", HorsePower = 200},
+        new Vehicle { Id = 3, Manufacturer = "Toyota", Model = "Yaris", Color = "Red", HorsePower = 80},
+        new Vehicle { Id = 4, Manufacturer = "Volkswagen", Model = "Golf", Color = "White", HorsePower = 110}
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Vehicle>> GetAll()
+    {
+        return _vehicles;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Vehicle> Get(int id)
+    {
+        var vehicle = _vehicles.SingleOrDefault(x => x.Id == id);
+
+        if (vehicle == default)
+        {
+            return NotFound();
+        }
+        return vehicle;
+    }
+}
--- Example/Example.WebApi/Vehicle.cs
+++ Example/Example.WebApi/Vehicle.cs
@@ -3,8 +3,8 @@
 public class Vehicle
 {
     public int Id { get; set; }
-    public string Manufacturer { get; set; }
-    public string Model { get; set; }
-    public string Color { get; set; }
+    public string? Manufacturer { get; set; }
+    public string? Model { get; set; }
+    public string? Color { get; set; }
     public int HorsePower { get; set; }
 }
