c3c35efe9594cd40530ab7c44400914157f2c98b
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-05-29 10:56:20 +0200

    add vehicle model

---
+++ Example/Example.WebApi/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace Example.WebApi;
+
+public class Vehicle
+{
+    public int Id { get; set; }
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int HorsePower { get; set; }
+}
